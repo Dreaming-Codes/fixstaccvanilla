3989c2a97f5de2e70030db9e6b64a765446bc5cb
DreamingCodes <me@dreaming.codes>
2025-01-26 00:00:08 +0100

    Revert fences fix

--- src/main/java/codes/dreaming/fixstaccvanilla/FixStaccClient.java
+++ src/main/java/codes/dreaming/fixstaccvanilla/FixStaccClient.java
@@ -11,28 +11,19 @@ import net.minecraft.util.Identifier;
 
 public class FixStaccClient implements ClientModInitializer {
     public static boolean IsStacced = true;
-    public static boolean IsFenced = true;
 
     public static final String MOD_ID = "fixstacc";
 
     public static final Identifier ANNOUNCE_STACC_PACKET_ID = new Identifier(MOD_ID, "announce");
-    public static final Identifier ANNOUNCE_DIAGONALFENCES_PACKET_ID = new Identifier(MOD_ID, "fences");
 
     @Override
     public void onInitializeClient() {
         ClientPlayNetworking.registerGlobalReceiver(ANNOUNCE_STACC_PACKET_ID, FixStaccClient::onAnnounceStacc);
-        ClientPlayNetworking.registerGlobalReceiver(ANNOUNCE_DIAGONALFENCES_PACKET_ID, FixStaccClient::onAnnounceFences);
     }
 
     private static void onAnnounceStacc(MinecraftClient minecraftClient, ClientPlayNetworkHandler clientPlayNetworkHandler, PacketByteBuf packetByteBuf, PacketSender packetSender) {
         byte isStacced = packetByteBuf.getByte(0);
 
         IsStacced = isStacced == (byte) 1;
-    }
-
-    private static void onAnnounceFences(MinecraftClient minecraftClient, ClientPlayNetworkHandler clientPlayNetworkHandler, PacketByteBuf packetByteBuf, PacketSender packetSender) {
-        byte isFenced = packetByteBuf.getByte(0);
-
-        IsFenced = isFenced == (byte) 1;
     }
 }
